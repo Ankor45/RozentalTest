80356a6295c70c157d8bf77a8b6d3124ed85d7a5
Ankor <ankor_socialki@mail.ru>
2024-08-23 19:44:34 +0300

    - add AppColors

---
+++ RozentalTest/Resourses/AppColor.swift
@@ -0,0 +1,18 @@
+//
+//  AppColor.swift
+//  RozentalTest
+//
+//  Created by Andrei Kovryzhenko on 23.08.2024.
+//
+
+import Foundation
+
+import UIKit
+
+enum AppColors {
+    static let buttonOrange = UIColor().hex(0xF5C270)
+    static let gray = UIColor().hex(0x7E8188)
+    static let lightGray = UIColor().hex(0x9FA1AF)
+    static let black = UIColor.black
+    static let white = UIColor.white
+}
