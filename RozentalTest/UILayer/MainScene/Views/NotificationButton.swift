9c8236a03c02c7018347b784c1cd4db390739144
Ankor <ankor_socialki@mail.ru>
2024-08-31 01:02:03 +0300

    -add MainViewController -add NotificationButton -add profile info in mainview

---
+++ RozentalTest/UILayer/MainScene/Views/NotificationButton.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationButton.swift
+//  RozentalTest
+//
+//  Created by Andrei Kovryzhenko on 30.08.2024.
+//
+
+import UIKit
+
+class NotificationButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
